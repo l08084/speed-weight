a95ff1c958d1ff8388b129f026ad667cdc4ff390
l08084 <l08084.1989@gmail.com>
2020-09-25 20:15:07 +0900

    add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
     path: 'weight',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
