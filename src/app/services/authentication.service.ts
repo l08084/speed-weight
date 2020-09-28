aa415c42018c5d9301fb599dea2239f7ff11361e
l08084 <l08084.1989@gmail.com>
2020-09-28 22:30:08 +0900

    update

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,13 +2,27 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 
+/**
+ * 認証のサービスクラス
+ *
+ * @export
+ * @class AuthenticationService
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   constructor(public afAuth: AngularFireAuth) {}
 
-  public signInWithTwitter(): Promise<firebase.auth.UserCredential> {
-    return this.afAuth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+  /**
+   * Twitter認証を呼び出す
+   *
+   * @returns {Promise<void>}
+   * @memberof AuthenticationService
+   */
+  public signInWithTwitter(): Promise<void> {
+    return this.afAuth.signInWithRedirect(
+      new firebase.auth.TwitterAuthProvider()
+    );
   }
 }
