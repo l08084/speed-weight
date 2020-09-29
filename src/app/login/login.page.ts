23a8b8f5c248d18d6702be71aa40db6124f36f55
l08084 <l08084.1989@gmail.com>
2020-09-29 18:58:06 +0900

    update

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public async signInWithTwitter() {
+    try {
+      await this.authenticationService.signInWithTwitter();
+      this.router.navigate(['/weight/tabs/tab1']);
+    } catch (error) {
+      console.log(error);
+    }
   }
-
 }
