7df00b69ade37762320ff80e756a77433bbc988e
l08084 <l08084.1989@gmail.com>
2020-10-30 18:20:44 +0900

    利用規約画面とプライバシーポリシー画面を追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,14 @@ const routes: Routes = [
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
+  {
+    path: 'terms-of-service',
+    loadChildren: () => import('./terms-of-service/terms-of-service.module').then( m => m.TermsOfServicePageModule)
+  },
+  {
+    path: 'privacy-policy',
+    loadChildren: () => import('./privacy-policy/privacy-policy.module').then( m => m.PrivacyPolicyPageModule)
+  },
 ];
 @NgModule({
   imports: [
