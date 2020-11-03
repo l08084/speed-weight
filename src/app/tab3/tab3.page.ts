b8cf9e97a21bafd4589fc41287233c6486cf8481
l08084 <l08084.1989@gmail.com>
2020-11-03 21:22:04 +0900

    設定画面に利用規約とプライバシーポリシーのリンクを追加

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+/**
+ * 設定タブ画面
+ *
+ * @export
+ * @class Tab3Page
+ */
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +14,24 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  /**
+   * 利用規約画面に遷移する
+   *
+   * @memberof Tab3Page
+   */
+  public goToTermsOfService(): void {
+    this.router.navigateByUrl('/terms-of-service');
+  }
+
+  /**
+   * プライバシーポリシー画面に遷移する
+   *
+   * @memberof Tab3Page
+   */
+  public goToPrivacyPolicy(): void {
+    this.router.navigateByUrl('/privacy-policy');
+  }
 
 }
