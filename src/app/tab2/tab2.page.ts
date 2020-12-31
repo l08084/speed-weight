bf80cd72041b2d85a247dd56c83246b41af455c3
l08084 <l08084.1989@gmail.com>
2020-12-31 22:26:10 +0900

    add comment

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -56,6 +56,12 @@ export class Tab2Page {
     });
   }
 
+  /**
+   * 期間タブをクリックする
+   *
+   * @param {Period} period
+   * @memberof Tab2Page
+   */
   public clickPeriodTab(period: Period) {
     console.log(period);
     this.selectedPeriodTab = period;
